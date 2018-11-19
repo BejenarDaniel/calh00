57a52f81bd957f74a48c1f1a5cf62c147f9e7a36
Tanas Leonid <cool.tanas@gmail.com>
2018-11-19 16:06:51 +0200

    Add MCAL

---
+++ sys_tasks.c
@@ -0,0 +1,46 @@
+/* 
+ * File:   sys_tasks.h
+ * Author: Cristian T. A.
+ *
+ * Created on August 17, 2018, 1:26 PM
+ */
+
+#include "general.h"
+#include "sys_tasks.h"
+
+extern BOOL bObstDetected;
+
+void TASK_Inits()
+{
+
+}
+
+void TASK_1ms()
+{
+    
+}
+
+void TASK_5ms()
+{
+
+}
+
+void TASK_10ms()
+{   
+
+}
+
+void TASK_100ms()
+{ 
+    
+}
+
+void TASK_500ms()
+{ 
+
+}
+
+void TASK_1000ms()
+{
+
+}
